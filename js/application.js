c41531ecad7dc6dbbb12219600059e9a9b6147a5
Jamie Reardon <jamie.reardon91@gmail.com>
2022-09-21 15:46:14 +0100

    refactor app

--- js/application.js
+++ js/application.js
@@ -1,93 +1,65 @@
-const randomUserAPI = 'https://randomuser.me/api/?results=12&nat=us,gb';
 const search = document.getElementById('search');
 const sectionGrid = document.querySelector('.grid');
-const cards = document.getElementsByClassName('card');
+const cards = document.querySelectorAll('.card');
 const overlay = document.querySelector('.overlay');
 const modalContainer = document.querySelector('.modal__content');
 const modalClose = document.querySelector('.modal__close');
+const modalBtns = document.querySelectorAll('.modal button');
 const modalPrevious = document.querySelector('.modal__left');
 const modalNext = document.querySelector('.modal__right');
 let employeeData = [];
-
-// ---------------------------------------------
-//  FETCH FUNCTIONS
-// ---------------------------------------------
-
-const fetchData = url => {
-  return fetch(url)
-           .then(res => res.json())
-           .then(data => generateEmployees(data))
-           .catch(err => console.log(err))
-}
-
-fetchData(randomUserAPI);
 
 // ---------------------------------------------
 //  HELPER FUNCTIONS
 // ---------------------------------------------
 
 const generateEmployees = data => {
-  employeeData = data;
+  employeeData = data.results;
   let html = ``;
-  // loop over all the data and display each peice
-  for (let i=0; i<data.results.length; i+=1) {
-    let photo = data.results[i].picture.large;
-    let name = data.results[i].name.first + ' ' + data.results[i].name.last;
-    let email = data.results[i].email;
-    let city = data.results[i].location.city;
-    html += `
-      <div class="card" data-index="${i}">
-        <div class="card__image">
-        <img src="${photo}" alt="Picture of Employee: ${name}" class="photo">
-        </div>
-        <div class="card__details">
-          <h2 class="card__name">${name}</h2>
-          <p class="card__email">${email}</p>
-          <p class="card__location">${city}</p>
-        </div>
-      </div>
-    `;
-  }
+
+  employeeData.forEach((employee, i) => {
+    const { name: { first, last }, email, location: { city }, picture } = employee;
+    const name = `${first} ${last}`;
+
+    html += `<section class="card" data-index=${i}>
+      <section class="card__image">
+        <img class="photo" src=${picture.large} alt="Picture of Employee: ${name}">
+      </section>
+      <section class="card__details">
+        <h2 class="card__name">${name}</h2>
+        <p class="card__email">${email}</p>
+        <p class="card__location">${city}</p>
+      </section>
+    </section>`;
+  });
+
   sectionGrid.innerHTML = html;
 }
 
 const displayModal = index => {
-  let photo = employeeData.results[index].picture.large;
-  let name = employeeData.results[index].name.first + ' ' + employeeData.results[index].name.last;
-  let email = employeeData.results[index].email;
-  let city = employeeData.results[index].location.city;
-  let phone = employeeData.results[index].cell;
-  let address = employeeData.results[index].location.street.number + ' ' + employeeData.results[index].location.street.name + ', ' + employeeData.results[index].location.state + ' ' + employeeData.results[index].location.postcode;
-  let dob = employeeData.results[index].dob;
-  let dobFull = new Date(dob.date);
-  const html = `
-    <div class="card__image">
-      <img src="${photo}" alt="Picture of Employee: ${name}" class="photo">
-    </div>
-    <div class="card__details">
+  const { name: { first, last }, email, location, picture, cell, dob } = employeeData[index];
+  const name = `${first} ${last}`;
+  const address = `${location.street.number} ${location.street.name} ${location.state} ${location.postcode}`;
+  const dobFull = new Date(dob.date);
+  const html = `<section class="card__image">
+      <img src="${picture.large}" alt="Picture of Employee: ${name}" class="photo">
+    </section>
+    <section class="card__details">
       <h2 class="card__name">${name}</h2>
       <p class="card__email">${email}</p>
-      <p class="card__location">${city}</p>
+      <p class="card__location">${location.city}</p>
       <hr>
-      <p class="card__phone">${phone}</p>
+      <p class="card__phone">${cell}</p>
       <p class="card__address">${address}</p>
       <p class="card__birthday">${dobFull.getDate()}/${dobFull.getMonth()}/${dobFull.getFullYear()}</p>
-    </div>
-  `;
+    </section>`;
+
   overlay.classList.remove('hidden');
   modalContainer.innerHTML = html;
   modalContainer.style.display = 'block';
   modalContainer.setAttribute('data-index', index);
-  if (parseInt(index) === 0) {
-    modalPrevious.style.display = 'none';
-  } else {
-    modalPrevious.style.display = 'inline-block';
-  }
-  if (parseInt(index) === 11) {
-    modalNext.style.display = 'none';
-  } else {
-    modalNext.style.display = 'inline-block';
-  }
+  modalPrevious.style.display = parseInt(index) === 0 ? 'none' : 'inline-block';
+  modalNext.style.display = parseInt(index) === employeeData.length - 1 ? 'none' : 'inline-block';
 }
 
 const getTargetCard = target => {
@@ -97,52 +69,37 @@ const getTargetCard = target => {
 }
 
 const searchHandler = () => {
-  let input = search.value.toLowerCase();
+  const input = search.value.toLowerCase();
   const employees = document.querySelectorAll('.card__name');
-  for (let i=0; i<employees.length; i+=1) {
-    let employee = employees[i].textContent.toLowerCase();
-    let card = employees[i].parentNode.parentNode;
-    if (employee.indexOf(input) > -1) {
-      card.style.display = 'flex';
-    } else {
-      card.style.display = 'none';
-    }
-  }
-  if (input === '') {
-    for (let i=0; i<cards.length; i+=1) {
-      cards[i].style.display = 'flex';
-    }
-  }
+
+  employees.forEach(employee => {
+    const employeeName = employee.textContent.toLowerCase();
+    const card = employee.parentNode.parentNode;
+    card.style.display = employeeName.indexOf(input) > -1 ? 'flex' : 'none';
+  });
+
+  input === '' && cards.forEach(card => card.style.display = 'flex');
 }
 
 // ---------------------------------------------
 //  EVENT LISTENERS
 // ---------------------------------------------
 
-sectionGrid.addEventListener('click', e => {
-  if (e.target !== sectionGrid) {
-    getTargetCard(e.target);
-  }
-});
-
+sectionGrid.addEventListener('click', e => e.target !== sectionGrid && getTargetCard(e.target));
 search.addEventListener('keyup', searchHandler);
-
 modalClose.addEventListener('click', () => overlay.classList.add('hidden'));
 
-modalPrevious.addEventListener('click', e => {
-  const card = e.target.nextElementSibling.nextElementSibling;
-  let index = parseInt(card.getAttribute('data-index'));
-  index -= 1;
-  if (index !== -1) {
-    displayModal(index);
-  }
+modalBtns.forEach(btn => {
+  btn.addEventListener('click', () => {
+    const card = btn.className === 'modal__left' ? btn.nextElementSibling.nextElementSibling : btn.nextElementSibling;
+    let index = parseInt(card.getAttribute('data-index'));
+    index += btn.className === 'modal__left' ? -1 : +1;
+    
+    if (index !== -1 && index !== employeeData.length) displayModal(index);
+  });
 });
 
-modalNext.addEventListener('click', e => {
-  const card = e.target.nextElementSibling;
-  let index = parseInt(card.getAttribute('data-index'));
-  index += 1;
-  if (index !== 12) {
-    displayModal(index);
-  }
-});
+fetch('https://randomuser.me/api/?results=12&nat=us,gb')
+  .then(res => res.json())
+  .then(generateEmployees)
+  .catch(err => console.log(err));
